53314ad90a320e250ce1223cb000aff52e4218ed
Mike Cronce <mike@cronce.io>
2020-01-30 10:48:53 -0500

    src/user.rs:  Refactored all the handlers with common::ResponderResult and #[responder]

--- src/user.rs
+++ src/user.rs
@@ -27,48 +27,40 @@ impl mysql::prelude::FromRow for User /* {{{ */ {
 	}
 } // }}}
 
-pub(crate) async fn create(user: actix_web::web::Json<User>, state: common::State) -> common::HandlerResult /* {{{ */ {
-	db_handler!(state, conn, {
-		let mut user = user.into_inner();
-		let result = conn.prep_exec("INSERT INTO users VALUES (DEFAULT, ?)", params!(&user.name))?;
-		user.id = result.last_insert_id() as u32;
-		Ok::<User, mysql::Error>(user)
-	})
+#[responder]
+pub(crate) async fn create(user: actix_web::web::Json<User>, state: common::State) -> common::ResponderResult<User> /* {{{ */ {
+	let mut user = user.into_inner();
+	let result = query!(state.db, "INSERT INTO users VALUES (DEFAULT, ?)", &user.name);
+	user.id = result.last_insert_id() as u32;
+	Ok(json!(user))
 } // }}}
 
-pub(crate) async fn get_all(state: common::State) -> common::HandlerResult /* {{{ */ {
-	db_handler!(state, conn, {
-		let result = conn.prep_exec("SELECT * FROM users", ())?;
-		let users = result.map(|row| {
-			let row = row.unwrap();
-			mysql::from_row(row)
-		}).collect();
-		Ok::<Vec<User>, mysql::Error>(users)
-	})
+#[responder]
+pub(crate) async fn get_all(state: common::State) -> common::ResponderResult<Vec<User>> /* {{{ */ {
+	let result = query!(state.db, "SELECT * FROM users");
+	let users = common::collect(result);
+	Ok(json!(users))
 } // }}}
 
-pub(crate) async fn get_single(id: common::Path<u32>, state: common::State) -> common::HandlerResult /* {{{ */ {
-	db_handler!(state, conn, {
-		let mut result = conn.prep_exec("SELECT * FROM users WHERE id = ?", params!(*id))?;
-		let row = result.next().unwrap()?;
-		let user = mysql::from_row(row);
-		Ok::<User, mysql::Error>(user)
-	})
+#[responder]
+pub(crate) async fn get_single(id: common::Path<u32>, state: common::State) -> common::ResponderResult<User> /* {{{ */ {
+	let mut result = query!(state.db, "SELECT * FROM users WHERE id = ?", *id);
+	let row = result.next().unwrap()?;
+	let user = mysql::from_row(row);
+	Ok(json!(user))
 } // }}}
 
-pub(crate) async fn update(id: common::Path<u32>, user: actix_web::web::Json<User>, state: common::State) -> common::HandlerResult /* {{{ */ {
-	db_handler!(state, conn, {
-		let mut user = user.into_inner();
-		user.id = id.into_inner();
-		conn.prep_exec("UPDATE users SET name = ? WHERE id = ?", params!(&user.name, &user.id))?;
-		Ok::<User, mysql::Error>(user)
-	})
+#[responder]
+pub(crate) async fn update(id: common::Path<u32>, user: actix_web::web::Json<User>, state: common::State) -> common::ResponderResult<User> /* {{{ */ {
+	let mut user = user.into_inner();
+	user.id = *id;
+	query!(state.db, "UPDATE users SET name = ? WHERE id = ?", &user.name, &user.id);
+	Ok(json!(user))
 } // }}}
 
-pub(crate) async fn delete(id: common::Path<u32>, state: common::State) -> common::HandlerResult /* {{{ */ {
-	db_handler!(state, conn, {
-		conn.prep_exec("DELETE FROM users WHERE id = ?", params!(*id))?;
-		Ok::<bool, mysql::Error>(true)
-	})
+#[responder]
+pub(crate) async fn delete(id: common::Path<u32>, state: common::State) -> common::ResponderResult<bool> /* {{{ */ {
+	query!(state.db, "DELETE FROM users WHERE id = ?", *id);
+	Ok(json!(true))
 } // }}}
 
