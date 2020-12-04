0a6bf351ac95232700da364d8a42df245ef5d316
Mike Cronce <mike@cronce.io>
2020-12-04 15:40:06 -0500

    src/common.rs:  rm all the helper stuff for `mysql` crate; turns out we were basically building `sqlx::query!()` but not nearly as good.

--- src/common.rs
+++ src/common.rs
@@ -16,49 +16,6 @@ pub(crate) struct WebState {
 pub(crate) type Path<T> = actix_web::web::Path<T>;
 pub(crate) type State = actix_web::web::Data<WebState>;
 
-macro_rules! params {
-	() => { mysql::Params::Empty };
-	($($input: expr),+) => {{
-		let mut vector = Vec::new();
-		$(vector.push(mysql::Value::from($input));)+
-		mysql::Params::Positional(vector)
-	}};
-}
-
-macro_rules! query {
-	($db: expr, $query: literal $(, $params: expr)*) => {{
-		let db = $db.clone();
-		let params = params!($($params),*);
-		let future = actix_web::web::block(move || {
-			db.prep_exec($query, params)
-		});
-		match future.await {
-			Ok(v) => v,
-			Err(e) => {
-				println!("!!! {}", e);
-				return Ok(code!(InternalServerError));
-			}
-		}
-	}}
-}
-
-macro_rules! col {
-	($row: ident, $i: literal, $type: ty) => {{
-		let result = $row.take::<$type, _>($i);
-		if(result.is_none()) {
-			return Err(Box::new(common::missing_column_error($row, $i)));
-		}
-		result.unwrap()
-	}}
-}
-
-pub(crate) fn collect<T: mysql::prelude::FromRow>(result: mysql::QueryResult) -> Vec<T> {
-	result.map(|row| {
-		let row = row.unwrap();
-		mysql::from_row(row)
-	}).collect()
-}
-
 pub(crate) fn zerotime() -> chrono::NaiveDateTime {
 	chrono::NaiveDateTime::from_timestamp(0, 0)
 }
