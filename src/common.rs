0bff9ce4ed3356249d268953f83ccf64aacb2b5e
Mike Cronce <mike@cronce.io>
2020-12-04 16:15:51 -0500

    src/common.rs:  mysql -> sqlx

--- src/common.rs
+++ src/common.rs
@@ -1,13 +1,13 @@
 extern crate actix_web;
 extern crate chrono;
-extern crate mysql;
+extern crate sqlx;
 extern crate serde;
 
 pub(crate) use actix_helper_macros::*;
 
 #[derive(Clone)]
 pub(crate) struct WebState {
-	pub(crate) db: mysql::Pool
+	pub(crate) db: sqlx::mysql::MySqlPool
 }
 
 pub(crate) type Path<T> = actix_web::web::Path<T>;
